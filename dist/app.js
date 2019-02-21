00dbe9c55000dc0ca41060d2a27b9423bc6de92e
Rostyslav Yashkin <pan_rostislav@ukr.net>
2019-02-21 15:58:04 +0200

    add controller, add ng-repeat with currencies on options

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,20 @@
-// console.log(5);
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+const app = angular.module('CurrencyExchanger', []);
+
+app.controller('CurController', function($scope, $http) {
+  $scope.list = [];
+
+  $http({
+    method: 'GET',
+    url: 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
+  }).then(response => {
+    response.data.forEach(element => {
+      $scope.list.push(element);
+    });
+  }, err => {
+    console.log(err);
+  });
+});
 const a = 10;
 // console.log(a + 2);
